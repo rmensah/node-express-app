b2adcef7d925064a9edd3f337dcff9d1595c617f
RM <rm@RMs-MacBook-Pro.fios-router.home>
2016-02-15 21:58:46 -0500

    debugged mysql code

--- burger/db/schema.sql
+++ burger/db/schema.sql
@@ -5,10 +5,11 @@ USE burgers_db;
 
 CREATE TABLE burgers
 (
-	id int NOT NULL AUTO_INCREMENT,
+	ID INT AUTO_INCREMENT NOT NULL,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOL,
-	date DEFAULT
+	date TIMESTAMP,
+	PRIMARY KEY (ID)
 );
 
 
--- burger/db/seeds.sql
+++ burger/db/seeds.sql
@@ -1,1 +1,1 @@
-INSERT INTO burgers (burger_name, devoured, date) VALUES (?, ?, ?);
+INSERT INTO burgers (burger_name, devoured, date) VALUES ('?');
